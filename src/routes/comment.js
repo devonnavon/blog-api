0ca51225b3df13b65954be5650754c3aa55275aa
Devon Navon <devon.navon@gmail.com>
2020-07-10 16:50:15 -0700

    endpoints all work! need to implement auth

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -2,8 +2,45 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.comments));
+router.get('/', async (req, res) => {
+  const comments = await req.context.models.Comment.find();
+  return res.send(comments);
+});
+
+router.get('/:postId', async (req, res) => {
+  const comments = await req.context.models.Comment.find({
+    post: req.params.postId,
+  });
+  return res.send(comments);
+});
+
+router.post('/:postId', async (req, res) => {
+  const comment = await req.context.models.Comment.create({
+    text: req.body.text,
+    user: req.context.me.id,
+    post: req.params.postId,
+  });
+
+  return res.send(comment);
+});
+
+router.delete('/:commentId', async (req, res) => {
+  const comment = await req.context.models.Comment.findById(
+    req.params.commentId,
+  );
+
+  if (comment) {
+    await comment.remove();
+  }
+
+  return res.send(comment);
 });
 
 export default router;
+
+//curl http://localhost:3000/comments
+//curl http://localhost:3000/comments/5f08fd7993f784265779f6ca
+//curl -X POST -H "Content-Type:application/json" http://localhost:3000/comments/5f08fd7993f784265779f6ca/ -d '{"text":"please work"}'
+//curl http://localhost:3000/comments/5f08fd7993f784265779f6ca
+//curl -X DELETE http://localhost:3000/comments/<>
+//curl http://localhost:3000/comments/5f08fd7993f784265779f6ca
